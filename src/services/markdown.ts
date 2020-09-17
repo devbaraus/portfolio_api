2a208402aadbcfe5301c9c01ddceb16dc8f9012d
Bruno de Araujo Alves  devbaraus <contato@baraus.dev>
2020-09-17 09:58:22 -0300

    Teste Open Graphic Data

--- src/services/markdown.ts
+++ src/services/markdown.ts
@@ -1,8 +1,25 @@
 import showdown from 'showdown'
 import textversionjs from 'textversionjs'
 
+// const showdown = require('showdown')
+// const textversionjs = require('textversionjs')
+
 function markdown() {
-  return new showdown.Converter()
+  return new showdown.Converter({
+    omitExtraWLInCodeBlocks: true,
+    ghCompatibleHeaderId: true,
+    simplifiedAutoLink: true,
+    strikethrough: true,
+    tables: true,
+    ghCodeBlocks: true,
+    smartIndentationFix: true,
+    simpleLineBreaks: true,
+    ghMentions: true,
+    encodeEmails: true,
+    openLinksInNewWindow: true,
+    emoji: true,
+    literalMidWordUnderscores: true,
+  })
 }
 
 export function getTextFromMarkdown(input: string) {
